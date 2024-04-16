f54be37232df9182aebad00b3b349ff62bbc3107
Akshay <akshaymashokan@gmail.com>
2024-04-16 18:49:04 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://akshy3.github.io",
+  base: "astro-template",
   integrations: [
     tailwind(),
     sitemap({
